f0b7311a902e801f00a542d3117fd19296bd5fb4
Postmodern <postmodern.mod3@gmail.com>
2021-12-20 01:30:25 -0800

    Style.

--- lib/agent.rb
+++ lib/agent.rb
@@ -420,12 +420,9 @@ if $0 == __FILE__
   end
 
   case ARGV[0]
-  when '--http'
-    Agent::HTTP::Server.start ARGV[1], ARGV[2]
-  when '--listen'
-    Agent::TCP::Server.start ARGV[1], ARGV[2]
-  when '--connect'
-    Agent::TCP::ConnectBack.start ARGV[1], ARGV[2]
+  when '--http'    then Agent::HTTP::Server.start(ARGV[1],ARGV[2])
+  when '--listen'  then Agent::TCP::Server.start(ARGV[1],ARGV[2])
+  when '--connect' then Agent::TCP::ConnectBack.start(ARGV[1],ARGV[2])
   else
     usage
   end
