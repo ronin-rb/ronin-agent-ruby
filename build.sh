ab2c5df7cd8fcae15422a9bdbde1bac0d15b0557
Postmodern <postmodern.mod3@gmail.com>
2022-10-26 21:00:57 -0700

    Quote all variables.

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 output="${1:-agent.rb}"
 
-echo "#!/usr/bin/env ruby" > $output
-cat lib/agent/rpc/*.rb >> $output
-cat lib/agent/rpc.rb >> $output
-cat lib/agent/message.rb >> $output
-cat lib/agent/tcp/protocol.rb >> $output
-cat lib/agent/tcp/connect_back.rb >> $output
-cat lib/agent/tcp/gserver.rb >> $output
-cat lib/agent/http/server.rb >> $output
-cat lib/agent/main.rb >> $output
-chmod +x $output
+echo "#!/usr/bin/env ruby" > "$output"
+cat lib/agent/rpc/*.rb >> "$output"
+cat lib/agent/rpc.rb >> "$output"
+cat lib/agent/message.rb >> "$output"
+cat lib/agent/tcp/protocol.rb >> "$output"
+cat lib/agent/tcp/connect_back.rb >> "$output"
+cat lib/agent/tcp/gserver.rb >> "$output"
+cat lib/agent/http/server.rb >> "$output"
+cat lib/agent/main.rb >> "$output"
+chmod +x "$output"
